a38c8496ebc908df8c8a30a60b299590e32099aa
Amitesh Rai <abhiapor543@gmail.com>
2023-10-09 19:42:24 +0530

    v

--- todo/models.py
+++ todo/models.py
@@ -2,6 +2,11 @@ from django.db import models
 
 class Todo(models.Model):
     title=models.CharField(max_length=1000)
+    created=models.DateField(auto_now_add=True)
+    updated=models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        ordering=['-updated','-created']
 
 
     def __str__(self):
